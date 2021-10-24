3697653c446cfab532f877d771e9e0104ed958d0
KAVAN-DESAI <62831469+KAVAN-DESAI@users.noreply.github.com>
2021-10-24 14:48:09 +0530

    Update SortingAlgorithmVisuilizer.py

--- SortingAlgorithmVisuilizer.py
+++ SortingAlgorithmVisuilizer.py
@@ -17,3 +17,23 @@ arr_clr = [(0, 204, 102)]*(sz+1)
 clr_ind = 0
 clr = [(0, 204, 102), (255, 0, 0),
        (0, 0, 153), (255, 102, 0),(230,230,250)]
+
+
+fnt = pygame.font.SysFont("comicsans", 30)
+fnt1 = pygame.font.SysFont("comicsans", 30)
+fnt2 = pygame.font.SysFont("comicsans", 20)
+
+
+base_font = pygame.font.Font(None, 32)
+user_text = ''
+
+input_rect = pygame.Rect(600, 10, 140, 32)
+  
+
+color_active = pygame.Color('lightskyblue3')
+  
+
+color_passive = pygame.Color('chartreuse4')
+color = color_passive
+  
+active = False
