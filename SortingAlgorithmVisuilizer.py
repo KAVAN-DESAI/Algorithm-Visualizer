627ee27dc2573239d0c9ea21549af7ecb7f0827c
KAVAN-DESAI <62831469+KAVAN-DESAI@users.noreply.github.com>
2021-10-24 14:47:36 +0530

    Update SortingAlgorithmVisuilizer.py

--- SortingAlgorithmVisuilizer.py
+++ SortingAlgorithmVisuilizer.py
@@ -1,1 +1,19 @@
 import pygame
+import random
+pygame.font.init()
+
+
+screen = pygame.display.set_mode((900, 650))
+  
+pygame.display.set_caption("SORTING VISUALISER")
+
+run = True
+
+width = 900
+length = 600
+sz = 50000
+array = [0]*(sz+1)
+arr_clr = [(0, 204, 102)]*(sz+1)
+clr_ind = 0
+clr = [(0, 204, 102), (255, 0, 0),
+       (0, 0, 153), (255, 102, 0),(230,230,250)]
