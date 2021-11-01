046e42b10e33bf70898695c85d420a880a4fb9f0
KAVAN-DESAI <62831469+KAVAN-DESAI@users.noreply.github.com>
2021-11-01 08:10:31 +0530

    Update SortingAlgorithmVisuilizer.py

--- SortingAlgorithmVisuilizer.py
+++ SortingAlgorithmVisuilizer.py
@@ -180,3 +180,35 @@ def mergesort(array, l, r):
       mergesort(array, l, mid)
       mergesort(array, mid + 1, r)
       merge(array, l, mid,mid + 1, r)
+
+def merge(array, x1, y1, x2, y2):
+    i = x1
+    j = x2
+    temp = []
+    pygame.event.pump()
+    while i <= y1 and j <= y2:
+        arr_clr[i] = clr[1]
+        arr_clr[j] = clr[1]
+        refill(1)
+        arr_clr[i] = clr[0]
+        arr_clr[j] = clr[0]
+        if array[i] < array[j]:
+            temp.append(array[i])
+            i += 1
+        else:
+            temp.append(array[j])
+            j += 1
+    while i <= y1:
+        arr_clr[i] = clr[1]
+        refill(1)
+        arr_clr[i] = clr[0]
+        temp.append(array[i])
+        i += 1
+    while j <= y2:
+        arr_clr[j] = clr[1]
+        refill(1)
+        arr_clr[j] = clr[0]
+        temp.append(array[j])
+        j += 1
+    j = 0
+    
